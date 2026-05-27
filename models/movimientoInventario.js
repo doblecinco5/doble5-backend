9182dd9aeea2e95e0895a98dde09c8a94bf92da0
doblecinco <doblecinco55store@gmail.com>
2026-05-27 10:52:34 -0300

    refactor(models): better use of curve-based income

--- models/movimientoInventario.js
+++ models/movimientoInventario.js
@@ -1,69 +1,73 @@
 const { Schema, model } = require('mongoose');
 
-const MovimientoInventarioSchema = new Schema(
-    {
-        producto: {
-            type: Schema.Types.ObjectId,
-            ref: 'Producto',
-            required: true
-        },
+const DetalleMovimientoSchema = new Schema({
 
-        nombreProducto: {
-            type: String,
-            required: true
-        },
-
-        productos: [
-            {
-                producto,
-                nombreProducto,
-                cantidadCurvas,
-                movimientoPorTalle
-            }
-        ],
-
-        tipo: {
-            type: String,
-            enum: [
-                'ingreso',
-                'venta_online',
-                'venta_presencial',
-                'ajuste'
-            ],
-            required: true
-        },
-
-        referenciaId: {
-            type: Schema.Types.ObjectId,
-            default: null
-        },
-
-        observaciones: {
-            type: String,
-            default: ''
-        },
-
-        creadoPor: {
-            type: Schema.Types.ObjectId,
-            ref: 'Usuario',
-            default: null
-        }
+    talle: {
+        type: String,
+        required: true
     },
-    {
-        timestamps: true
+
+    cantidad: {
+        type: Number,
+        required: true
     }
-);
 
-MovimientoInventarioSchema.index({
-    producto: 1
-});
+}, { _id: false });
 
-MovimientoInventarioSchema.index({
-    tipo: 1
-});
+const MovimientoInventarioSchema = new Schema({
 
-MovimientoInventarioSchema.index({
-    createdAt: -1
+    tipo: {
+        type: String,
+        enum: [
+            'ingreso',
+            'venta_online',
+            'venta_presencial',
+            'ajuste'
+        ],
+        required: true
+    },
+
+    producto: {
+        type: Schema.Types.ObjectId,
+        ref: 'Producto',
+        required: true
+    },
+
+    nombreProducto: {
+        type: String,
+        required: true
+    },
+
+    detalles: [DetalleMovimientoSchema],
+
+    referenciaId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+
+    modeloReferencia: {
+        type: String,
+        enum: [
+            'Ingreso',
+            'Orden',
+            'VentaPresencial'
+        ],
+        required: true
+    },
+
+    observaciones: {
+        type: String,
+        default: ''
+    },
+
+    creadoPor: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        default: null
+    }
+
+}, {
+    timestamps: true
 });
 
 module.exports = model(
